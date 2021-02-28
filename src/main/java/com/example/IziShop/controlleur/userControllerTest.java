1a185f6300fa1d2cbe56f23eda79f487681542ea
had <ayoub0had@gmail.com>
2021-02-28 12:17:34 +0100

    test : whenFindAll_returnUsersList

--- src/main/java/com/example/IziShop/controlleur/userControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.IziShop.controlleur;
-
-
-
-public class userControllerTest {
-
-}
--- src/test/java/com/example/IziShop/TestUser.java
+++ src/test/java/com/example/IziShop/TestUser.java
@@ -1,17 +1,32 @@
 package com.example.IziShop;
 
-import  org.junit.Assert;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import java.util.Arrays;
+import java.util.List;
 
-@SpringBootTest
-@ContextConfiguration(classes = ConfigUT.class)
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.example.IziShop.entity.User;
+import com.example.IziShop.reposetory.IUserReposetory;
+import com.example.IziShop.service.UserService;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TestUser {
+	@Mock
+	private IUserReposetory userReposerotry;
 
+	private UserService service;
 	@Test
-	public void MyTest() {
-		Assert.assertTrue(true);
+	public void whenFindAll_ReturnUsersList() {
+		User user = new User(1L,"ayoub","had","ayoub@haadcom","casa","05353566");
+		when(userReposerotry.findAll()).thenReturn(Arrays.asList(user));
+		List<User> users = service.getAll();
+		assertThat(users).hasSize(1);   
 	}
+
 }
